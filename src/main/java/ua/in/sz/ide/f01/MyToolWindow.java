8ec868c468adf0b54c54e13a9d3252d155223243
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2020-05-20 07:05:51 +0300

    Create IDEA plugin. Create tree

--- src/main/java/ua/in/sz/ide/f01/MyToolWindow.java
+++ src/main/java/ua/in/sz/ide/f01/MyToolWindow.java
@@ -3,7 +3,6 @@ package ua.in.sz.ide.f01;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.ui.treeStructure.SimpleTree;
 
-
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
@@ -12,28 +11,34 @@ public class MyToolWindow {
 
     private JPanel myToolWindowContent;
     private SimpleTree simpleTree1;
+    private DefaultTreeModel treeModel;
+    private DefaultMutableTreeNode rootNode;
 
     public MyToolWindow(ToolWindow toolWindow) {
         createRepositoryTree();
+        loadRepositories();
     }
 
     public JPanel getContent() {
         return myToolWindowContent;
     }
 
-    private DefaultTreeModel treeModel;
-    private DefaultMutableTreeNode rootNode;
-
     private void createRepositoryTree() {
         simpleTree1.setCellRenderer(new TreeCellRendererLivingDoc());
         simpleTree1.setRootVisible(true);
 
-        rootNode = new DefaultMutableTreeNode(getDefaultRootNode());
+        rootNode = new DefaultMutableTreeNode(new Node("Root node"));
         treeModel = new DefaultTreeModel(rootNode, true);
         simpleTree1.setModel(treeModel);
     }
 
-    private Node getDefaultRootNode() {
-        return new Node("Root node");
+    private void loadRepositories() {
+        for (int i = 0; i < 10; i++) {
+            Node moduleNode = new Node("child" + i);
+            DefaultMutableTreeNode moduleTreeNode = new DefaultMutableTreeNode(moduleNode, false);
+            rootNode.add(moduleTreeNode);
+        }
+
+        treeModel.reload();
     }
 }
